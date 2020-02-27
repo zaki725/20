dc964f6a8cf88ad58096b988e1b177fbf22116b6
zaki725 <zaki725725725@gmail.com>
2020-02-27 19:59:33 +0900

    add new file

---
+++ git2.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Git</title>
+</head>
+<body>
+<?php echo '2'; ?>
+</body>
+</html>
---
+++ git3.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Git</title>
+</head>
+<body>
+<?php echo '3'; ?>
+</body>
+</html>
